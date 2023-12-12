37541e542dbaab67f539895db22eea7f38bc6774
Amar <jeetamar496@gmail.com>
2023-12-12 22:01:15 +0530

    done

--- tours/src/App.js
+++ tours/src/App.js
@@ -4,8 +4,69 @@ import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([])
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
+
+ const fetchTours = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(url)
+      const tours = await response.json()
+      setLoading(false)
+      setTours(tours)
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+
+
+  useEffect(() => {
+    fetchTours()
+  }, [])
+
+  // if (loading) {
+  //   return (
+  //     <main>
+  //       <Loading />
+  //     </main>
+  //   )
+  // }
+
+
+
+// if don't want to use the other component in that case we can follow the below step, in have created the below this as a sperate component notours 
+
+  // if (tours.length === 0) { 
+  //   return (
+  //     <main>
+  //       <div className='title'>
+  //         <h2>no tours left</h2>
+  //         <button className='btn' onClick={() => fetchTours()}>
+  //           refresh
+  //         </button>
+  //       </div>
+  //     </main>
+  //   )
+  // }
+
+  
+  return (
+    <main>
+    {console.log("Loading:", loading)}
+    {loading ? <Loading /> : <Tours tours={tours} removeTour={removeTour} fetchTours={fetchTours} />}
+  </main>
+  
+  )
+
+
 }
 
 export default App
--- tours/src/Tours.js
+++ tours/src/Tours.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+import Notours from './Notours';
+const Tours = ({ tours, removeTour,fetchTours }) => {
+  return (
+
+    <>
+    {
+      tours.length === 0 ?  <Notours fetchTours={fetchTours}/> :
+   ( <section>
+    <div className="title">
+      <h2>our tours</h2>
+      <div className="underline"></div>
+    </div>
+    <div>
+      {tours.map((tour) => {
+        return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
+      })}
+    </div>
+    
+  </section>)
+}
+    </>
+  );
 };
 
 export default Tours;
